3b421706361091201bcc6eb3b9219c3e89c1fb5a
Wyclife Anyango <113285194+ApundaTheeShort@users.noreply.github.com>
2026-05-28 16:02:04 +0300

    Add aggregate queries for bookings and rankings
    Added aggregate queries for total bookings and rankings by property.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,3 +1,22 @@
+-- aggregate querry for total number of bookings made by each user
+
 SELECT COUNT(id) AS "count", user_id
 FROM bookings
 GROUP BY user_id;
+
+-- 
+
+-- SELECT p.id, p.name
+-- ROW_NUMBER() OVER(ORDER BY id DESC) AS popularity
+-- RANK() OVER(ORDER BY id DESC) AS rank
+-- FROM properties;
+
+
+--  aggregate queries for bookings and rankings
+SELECT 
+    PropertyID, 
+    COUNT(*) AS TotalBookings,
+    ROW_NUMBER() OVER(ORDER BY COUNT(*) DESC) AS Popularity
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS BookingRank
+FROM Bookings 
+GROUP BY PropertyID;
