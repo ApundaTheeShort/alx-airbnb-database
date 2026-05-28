d3f224835d97af46cc6315ef442fa5f42cb8a86e
Wyclife Anyango <113285194+ApundaTheeShort@users.noreply.github.com>
2026-05-28 15:19:43 +0300

    Add subqueries for property ratings and user bookings
    Add SQL queries to filter properties by average rating and user bookings.

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,3 +1,15 @@
+-- all properties whose avg rating is > 4.0
+
 SELECT p.id, p.title
 FROM properties p
 WHERE (SELECT AVG(rating) FROM reviews) > 4.0;
+
+
+SELECT outer.user_id 
+FROM bookings outer
+WHERE outer.user_id = inner.user_id
+(SELECT COUNT(inner.id),  inner.user_id 
+  FROM bookings inner 
+  GROUP BY inner.user_id 
+  HAVING COUNT(inner.id) > 3 
+  )
